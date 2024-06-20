80b999f59a04efd1846a5c5ba7d0f4cea3794e4c
stacihs <staci.holmessegarra@gmail.com>
2024-06-20 01:18:18 -0400

    Update app.js review.js inventory.js

--- server/database/app.js
+++ server/database/app.js
@@ -1,8 +1,9 @@
+/*jshint esversion: 8 */
 const express = require('express');
 const mongoose = require('mongoose');
 const fs = require('fs');
-const  cors = require('cors')
-const app = express()
+const  cors = require('cors');
+const app = express();
 const port = 3030;
 
 app.use(cors())
@@ -85,33 +86,6 @@ app.get('/fetchDealer/:id', async (req, res) => {
         res.status(500).json({ error: 'Error fetching documents' });
       }
 });
-
-//Express route to insert review
-// app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
-//   data = JSON.parse(req.body);
-//   const documents = await Reviews.find().sort( { id: -1 } )
-//   let new_id = documents[0]['id']+1
-
-//   const review = new Reviews({
-// 		"id": new_id,
-// 		"name": data['name'],
-// 		"dealership": data['dealership'],
-// 		"review": data['review'],
-// 		"purchase": data['purchase'],
-// 		"purchase_date": data['purchase_date'],
-// 		"car_make": data['car_make'],
-// 		"car_model": data['car_model'],
-// 		"car_year": data['car_year'],
-// 	});
-
-//   try {
-//     const savedReview = await review.save();
-//     res.json(savedReview);
-//   } catch (error) {
-// 		console.log(error);
-//     res.status(500).json({ error: 'Error inserting review' });
-//   }
-// });
 
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
     data = JSON.parse(req.body);
