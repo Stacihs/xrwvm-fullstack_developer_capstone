5298dc2aae0569147852cdd1996eb9d7f3775822
stacihs <staci.holmessegarra@gmail.com>
2024-06-21 18:16:55 -0400

    Fix Syntax

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = [
     'https://staciholmess-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01' +
     '.proxy.cognitiveclass.ai'
 ]
+
 CSRF_TRUSTED_ORIGINS = [
     'https://staciholmess-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01' +
     '.proxy.cognitiveclass.ai'
