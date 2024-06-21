57b3ff5568ba9180e874e6b251c7dcf8b76c25f3
Staci Holmes-Segarra <staci.holmessegarra@gmail.com>
2024-06-20 23:05:20 -0500

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,11 +30,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://staciholmess-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
-    ]
+    'https://staciholmess-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy
+    .cognitiveclass.ai'
+]
 CSRF_TRUSTED_ORIGINS = [
-    'https://staciholmess-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
-    ]
+    'https://staciholmess-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01
+    .proxy.cognitiveclass.ai'
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -99,7 +101,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation
+        .UserAttributeSimilarityValidator',
     },
     {
         'NAME':
